a45ca91415b190009550d130495159f2ba4ababa
Scriblestingray <kailoren101@gmail.com>
2026-03-24 01:16:07 -0700

    very basic logic, still bad

--- Config.c
+++ Config.c
@@ -5,8 +5,10 @@ struct UnitSystem CreateSystem() {
     FILE* fileptr = fopen("Config.txt", "r");
     fscanf(fileptr, "%s");
     fscanf(fileptr, "%s", sys.name);
+    int i = 0;
     while (fscanf(fileptr, "%s") != "-") {
-        fscanf(fileptr, "%s %s %s %s %s %s", sys.name);
+        fscanf(fileptr, "%s %d %s %d %d %s", sys.units[i].abbreviation, sys.units[i].is_base_unit, sys.units[i].base_unit, sys.units[i].multiplier, sys.units[i].offset, sys.units[i].quantity);
+        i++;
     }
     fclose(fileptr);
     return sys;
