475b71a7bed144c6dc20685572dd89f7623a099c
Scriblestingray <kailoren101@gmail.com>
2026-03-25 22:28:55 -0700

    Can now almost parse a single system

--- Config.c
+++ Config.c
@@ -1,15 +1,37 @@
 #include "structs.h"
+#include <stdio.h>
+#include <string.h>
 
 struct UnitSystem CreateSystem() {
     struct UnitSystem sys;
+    char buf[10];
+    char buf2[10];
+    char buf3[10];
+    int dec;
+    int dec2;
+    int dec3;
+    int dec4;
     FILE* fileptr = fopen("Config.txt", "r");
-    fscanf(fileptr, "%s");
+
+    //title of systen
     fscanf(fileptr, "%s", sys.name);
+
+
     int i = 0;
-    while (fscanf(fileptr, "%s") != "-") {
-        fscanf(fileptr, "%s %d %s %d %d %s", sys.units[i].abbreviation, sys.units[i].is_base_unit, sys.units[i].base_unit, sys.units[i].multiplier, sys.units[i].offset, sys.units[i].quantity);
+    fscanf(fileptr, "%s %d %s %d %d %d %s", buf, &dec, buf2, &dec2, &dec3, &dec4, buf3);
+    while (strcmp(buf, "-") != 0) {
+        sys.units[i].abbreviation = buf;
+        sys.units[i].is_base_unit = dec;
+        sys.units[i].base_unit = buf2; //fix this, not correct type, setting a struct to a string, not good
+        sys.units[i].multiplier = dec2;
+        sys.units[i].divider = dec3;
+        sys.units[i].offset = dec4;
+        sys.units[i].quantity = buf3; //also wrong type
+        fscanf(fileptr, "%s %d %s %d %d %d %s", buf, &dec, buf2, &dec2, &dec3, &dec4, buf3);
         i++;
     }
+
+    sys.unit_count = i;
     fclose(fileptr);
     return sys;
 }
